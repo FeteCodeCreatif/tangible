ebf66cc367b24ede54f1e61cccae2941359ec914
Romain Liblau <romain.liblau@magicmakers.fr>
2015-11-25 20:24:47 +0100

    code propre à moitié commenté

--- TapTapBox/tangible/tangible.js
+++ TapTapBox/tangible/tangible.js
@@ -4,42 +4,62 @@
 *                                              *
 ************************************************/
 
-var edit_cursor = 0;
-var exec_cursor = 0;
-var command_list = [
+/**************
+* Definitions *
+***************/
+var commands = ["box"]; // The buffer of current commands
+var editCursor = 0;  // the index of the command to be edited next
+var execCursor = 0; // the index of the command currently executed
+
+// Our list of available commands //
+var commandList = [
   "rotate blue box",
   "rotate white ball",
   "rotate red ball",
   "rotate move yellow box",
   "move black box"
-]
-var commands = ["box"];
+];
+
+var boucleExec = false; // to avoid an error on initialisation
 
 
+/************
+* Functions *
+*************/
 
-var curs2im = function(edit_cursor){
-  return '#im' + edit_cursor;
+// curs2im transforms the edition index into the id of the image in our visual
+//         buffer in index.html
+var curs2im = function(editCursor){
+  return '#im' + editCursor;
 };
 
-var send_command = function(command_index){
-  console.log((commands[command_index]));
-  top.frames[0].editor.setValue(commands[command_index]);
+// logBuffer logs the current state of our command buffer
+var logBuffer = function(){
+  console.log("Commands currently bufferized for execution : ");
+  console.log(commands);
 }
 
-var execute_buffer = function(){
-  while (!commands[exec_cursor]){
-    exec_cursor = (exec_cursor+1)%8
+// sendCommand sets the content of livecodelab's editor to the command in our
+//             command buffer at the index commandIndex
+var sendCommand = function(commandIndex){
+  top.frames[0].editor.setValue(commands[commandIndex]);
+}
+
+// executeBuffer sends to livecodelab's editor the current command defined by
+//               the index execCursor and
+var executeBuffer = function(){
+  while (!commands[execCursor]){
+    execCursor = (execCursor+1)%8
   }
-  console.log(exec_cursor);
-  send_command(exec_cursor);
-  exec_cursor = (exec_cursor+1)%8
-
+  sendCommand(execCursor);
+  execCursor = (execCursor+1)%8
 }
 
+var play = function(){
+  var boucleExec = setInterval(executeBuffer, 1000);
+}
 
-setInterval(execute_buffer, 1000);
-
-var clear = function(){
+var clearBuffer = function(){
   $('#im0').attr('src','vide.png');
   $('#im1').attr('src','vide.png');
   $('#im2').attr('src','vide.png');
@@ -48,57 +68,58 @@ var clear = function(){
   $('#im5').attr('src','vide.png');
   $('#im6').attr('src','vide.png');
   $('#im7').attr('src','vide.png');
-  edit_cursor = 0;
-  commands = [];
+  editCursor = 0;
+  commands = ["box"];
+  executeBuffer();
+  if(boucleExec){
+    clearInterval(boucleExec);
+  }
 };
+
+var initialisation = function(){
+  if(top.frames[0].editor){
+    clearBuffer();
+  }
+  else{
+    setTimeout(initialisation,100);
+  }
+}
+
+initialisation();
 
 $(document).keyup(function(touche){ // on écoute l'évènement keyup()
 
   var appui = touche.which || touche.keyCode; // le code est compatible tous navigateurs grâce à ces deux propriétés
-  var image = curs2im(edit_cursor);
+  var image = curs2im(editCursor);
 
   if(appui == 37){ // si le code de la touche est égal à 37 (Gauche)
-    //top.frames[0].editor.setValue("rotate blue box");
     $(image).attr('src','bounce.png');
-    commands[edit_cursor] = command_list[0];
-    //send_command(edit_cursor );
-    //execute_buffer();
-    console.log(commands);
-    edit_cursor = (edit_cursor + 1) %8;
+    commands[editCursor] = commandList[0];
+    logBuffer();
+    editCursor = (editCursor + 1) %8;
   }
   if(appui == 38){ // si le code de la touche est égal à 38 (Haut)
-    //top.frames[0].editor.setValue("rotate white ball");
     $(image).attr('src','flip.png');
-    commands[edit_cursor] = command_list[1];
-    //execute_buffer();
-    console.log(commands);
-    edit_cursor = (edit_cursor + 1) %8;
+    commands[editCursor] = commandList[1];
+    logBuffer();
+    editCursor = (editCursor + 1) %8;
   }
   if(appui == 39){ // si le code de la touche est égal à 39 (Droite)
-    //top.frames[0].editor.setValue("rotate red ball");
     $(image).attr('src','grow.png');
-    commands[edit_cursor] = command_list[2];
-    //execute_buffer();
-    console.log(commands);
-    edit_cursor = (edit_cursor + 1) %8;
+    commands[editCursor] = commandList[2];
+    logBuffer();
+    editCursor = (editCursor + 1) %8;
   }
   if(appui == 40){ // si le code de la touche est égal à 40 (Bas)
-    //top.frames[0].editor.setValue("rotate move yellow box");
     $(image).attr('src','zoom.png');
-    commands[edit_cursor] = command_list[3];
-    //execute_buffer();
-    console.log(commands);
-    edit_cursor = (edit_cursor + 1) %8;
-  }
-  if(appui == 32){ // si le code de la touche est égal à 31 (Espace)
-    //top.frames[0].editor.setValue("move black box");
-    $(image).attr('src','commande_6.png');
-    commands[edit_cursor] = command_list[4];
-    //execute_buffer();
-    console.log(commands);
-    edit_cursor = (edit_cursor + 1) %8;
+    commands[editCursor] = commandList[3];
+    logBuffer();
+    editCursor = (editCursor + 1) %8;
   }
   if(appui == 71){ // si le code de la touche est égal à 71 (g)
-    clear();
+    clearBuffer();
+  }
+  if(appui == 32){ // si le code de la touche est égal à 32 (Entrée)
+    play();
   }
 });
